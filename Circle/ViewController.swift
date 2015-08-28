af0a9a66eaeceb5fac53289f62c3c6d0bd453ca8
Akifumi Nishimura <akifumin@MBP-C02GP14CDV10.local>
2015-08-28 15:06:39 +0900

    first commit

---
+++ Circle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Circle
+//
+//  Created by Akifumi Nishimura on 2015/08/28.
+//  Copyright (c) 2015年 Akifumi Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.4'
+use_frameworks!
+
+pod 'SVProgressHUD', :head
+pod 'FlatUIKit'
